621237cff3b8b46af8c309cd9a2f16d3e7155d6c
LanceSpezzate <spezzate@gmail.com>
2017-06-16 17:25:43 +0300

    Subdomain fix for prodaction
    Subdomain fix for prodaction

--- src/Middleware/LangMiddleware.php
+++ src/Middleware/LangMiddleware.php
@@ -18,13 +18,13 @@ class LangMiddleware
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {	
+    {
 		preg_match('~setlocale~', url()->current(), $setlocale);
 		if(empty($setlocale)) {
-			$url_array = explode('--', parse_url($request->url(), PHP_URL_HOST));
-			if(empty($url_array)) $url_array = explode('.', parse_url($request->url(), PHP_URL_HOST));
+			//$url_array = explode('--', parse_url($request->url(), PHP_URL_HOST));
+			$url_array = explode('.', parse_url($request->url(), PHP_URL_HOST));
 			$subdomain = $url_array[0];
-			
+
 			if (in_array($subdomain, \Wbe\Crud\Models\ContentTypes\Languages::pluck('code')->toArray())) {
 				Session::put('locale', $subdomain);
 				Session::put('lang_id', \Wbe\Crud\Models\ContentTypes\Languages::where('code', $subdomain)->value('id'));
@@ -32,6 +32,7 @@ class LangMiddleware
 				Session::put('lang_id', \Wbe\Crud\Models\ContentTypes\Languages::where('code', Config::get('app.fallback_locale'))->value('id'));
 				Session::put('locale', Config::get('app.fallback_locale'));
 			}
+			//echo session('lang_id') . session('locale'); exit;
 			return $next($request);
 		}
 		return $next($request);
