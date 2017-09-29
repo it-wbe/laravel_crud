abd2745e95e9a0188ccc6a37ce18c164f3d88b1f
00rane00 <00rane00@gmail.com>
2017-09-29 16:06:42 +0300

    delete search in left menu

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -123,15 +123,6 @@
     </header>
     <acide class="main-sidebar">
         <section class="sidebar">
-            <form action="#" method="get" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search...">
-                    <span class="input-group-btn">
-					  <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-					  </button>
-					</span>
-                </div>
-            </form>
             @include('crud::common.vertical_menu')
         </section>
     </acide>
